9fc8d098f2de9932a3d4ef1d2265879dae1176ed
지현 고 <jihyun.ko@softpkorea.com>
2023-03-28 16:46:03 +0900

    초기 커밋.

---
+++ src/AE.Notice.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AE.Notice.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
